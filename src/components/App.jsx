b2114f17bcf6c1b17e3a40d790d14c723229d34f
hahmatovic1 <72711477+hahmatovic1@users.noreply.github.com>
2022-09-01 19:49:46 +0200

    feat: Add InputField component

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+function App() {
+    return <div class="container">
+            <h1>MY LIST</h1>
+            <InputField />
+            <div>
+                {/* display list */}
+            </div>
+
+    </div>
+}
+
+export default App;
